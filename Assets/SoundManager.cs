dfcedb603b623f00ecb437606f7bfc56b31c39b7
ValgardurRI <valgardur16@ru.is>
2018-02-13 22:01:38 +0000

    please don't be broken

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public List<AudioClip> sounds = new List<AudioClip>();
+
+	public AudioSource source;
+
+	public void PlaySound(AudioClip clip) {
+		source.PlayOneShot(clip);
+	}
+
+	public void PlayJump() {
+		PlaySound(sounds[0]);
+	}
+}
